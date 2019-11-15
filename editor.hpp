f7390d9aa71a13646340f7e34883c0cff73a47bf
csb6 <cblakley15@gmail.com>
2019-11-15 01:21:30 -0700

    Got rid of Editor class; added event handler so editor asks before quitting

--- editor.hpp
+++ editor.hpp
@@ -3,57 +3,44 @@
 #include <FL/Fl_Text_Editor.H>
 #include <FL/Fl_Text_Buffer.H>
 #include <FL/Fl_Native_File_Chooser.H>
-#include <string>
-#include <utility>
-#include <map>
+#include <FL/fl_ask.H>
 
-class Editor;
-typedef void (*Editor_Callback)(Editor*);
-struct Key_Bind {
-    std::pair<int,int> key; //{event_key, event_state}
-    Editor_Callback callback;
-};
+constexpr int Width = 500;
+// Name of editor font
+constexpr char Font[] = "Monaco";
+// Width of column on side of screen that contains line numbers
+constexpr int Line_Number_Width = 30; // in px
+// Size of editor buffer when program starts
+constexpr int Initial_Buffer_Size = 200; // in bytes?
 
-class Editor {
-private:
-    std::string m_filename;
-    Fl_Font m_font = 0;
-    void initFonts();
-    Fl_Text_Editor* m_editor;
-public:
-    static constexpr int Width = 500;
-    // Name of editor font
-    static constexpr char Font[] = "Monaco";
-    // Width of column on side of screen that contains line numbers
-    static constexpr int Line_Number_Width = 30; // in px
-    // Size of editor buffer when program starts
-    static constexpr int Initial_Buffer_Size = 200; // in bytes?
-    explicit Editor(int x, int y, const std::string filename);
-    void save(const char *filename);
-    void save() { save(m_filename.c_str()); };
-    void save_as();
-};
+static void save(Fl_Text_Editor *editor, const char *filename);
+static void save_as(Fl_Text_Editor *editor);
 
-constexpr Key_Bind Editor_Bindings[] = {
+
+constexpr Fl_Text_Editor::Key_Binding Editor_Bindings[] = {
     // Move cursor back One Word
-    {{'b', FL_COMMAND}, [](auto *editor) {
+    {'b', FL_COMMAND, [](int, auto *editor) {
 			    editor->previous_word();
+			    return 1;
 			}},
     // Move cursor forward One Word
-    {{'f', FL_COMMAND}, [](auto *editor) {
+    {'f', FL_COMMAND, [](int, auto *editor) {
 			   editor->next_word();
+			   return 1;
 		       }},
     // Search function
-    {{'s', FL_CONTROL}, [](auto *editor) {
-			  return;
+    {'s', FL_CONTROL, [](int, auto *editor) {
+			  return 1;
 		       }},
     // Save current buffer
-    {{'s', FL_COMMAND}, [](auto *editor) {
-			  editor->save();
+    {'s', FL_COMMAND, [](int, auto *editor) {
+			  save(editor, "test.txt");
+			  return 1;
 		       }},
     // Save As current buffer
-    {{'s', FL_COMMAND + FL_SHIFT}, [](auto *editor) {
-				      editor->save_as();
+    {'s', FL_COMMAND + FL_SHIFT, [](int, auto *editor) {
+				      save_as(editor);
+				      return 1;
 				   }}
 };
 #endif
--- main.cpp
+++ main.cpp
@@ -1,47 +1,45 @@
-#include <iostream>
 #include <cstring> //for strncmp()
 #include <FL/Fl.H>
 #include <FL/Fl_Window.H>
 #include "editor.hpp"
 
-Editor::Editor(int x, int y, const std::string filename)
-    : m_editor(x, y, Editor::Width, Editor::Width), m_filename(filename)
-{
-    buffer(new Fl_Text_Buffer(Initial_Buffer_Size));
-
-    cursor_style(Fl_Text_Display::HEAVY_CURSOR);
-    linenumber_width(Line_Number_Width);
-    wrap_mode(Fl_Text_Display::WRAP_AT_BOUNDS,
-	      Fl_Text_Display::WRAP_AT_COLUMN);
-
-    for(const auto &binding : Editor_Bindings) {
-	m_key_bindings[binding.key] = binding.callback;
-    }
-    initFonts();
-}
-
-void Editor::initFonts()
+static Fl_Font initFonts()
 {
     Fl_Font curr = 0;
     // Search for user-specified default font; try to set it
     while(Fl::get_font(curr) != nullptr) {
-	if(strncmp(Fl::get_font(curr), Editor::Font,
-		   sizeof(Editor::Font) / sizeof(char)) != 0) {
-	    Fl::set_font(curr, Editor::Font);
-	    m_font = curr;
-	    return;
+	if(strncmp(Fl::get_font(curr), Font,
+		   sizeof(Font) / sizeof(char)) != 0) {
+	    Fl::set_font(curr, Font);
+	    return curr;
 	}
 	++curr;
     }
+    return 0;
 }
 
-void Editor::save(const char *filename)
+static Fl_Text_Buffer* initEditor(Fl_Text_Editor *editor)
 {
-    auto *buf = buffer();
+    auto *buffer = new Fl_Text_Buffer(Initial_Buffer_Size);
+    editor->buffer(buffer);
+    editor->cursor_style(Fl_Text_Display::HEAVY_CURSOR);
+    editor->linenumber_width(Line_Number_Width);
+    editor->wrap_mode(Fl_Text_Display::WRAP_AT_BOUNDS,
+		      Fl_Text_Display::WRAP_AT_COLUMN);
+
+    for(const auto &binding : Editor_Bindings) {
+	editor->add_key_binding(binding.key, binding.state, binding.function);
+    }
+    return buffer;
+}
+
+static void save(Fl_Text_Editor *editor, const char *filename)
+{
+    auto *buf = editor->buffer();
     buf->savefile(filename, buf->length());
 }
 
-void Editor::save_as()
+static void save_as(Fl_Text_Editor *editor)
 {
     auto *chooser = new Fl_Native_File_Chooser(Fl_Native_File_Chooser::BROWSE_SAVE_FILE);
     int status = chooser->show();
@@ -50,22 +48,8 @@ void Editor::save_as()
 	delete chooser;
 	return;
     }
-    save(chooser->filename());
+    save(editor, chooser->filename());
     delete chooser;
-}
-
-int Editor::handle(int event) {
-    // Only override Fl_Text_Editor::handle() if the event is for a key
-    // press that maps to one of this Editor's bindings
-    const int state = Fl::event_state();
-    if(event == FL_KEYBOARD && (state == FL_COMMAND || state == FL_CONTROL)) {
-	const std::pair<int,int> key(Fl::event_key(), state);
-	if(m_key_bindings.count(key) >= 1) {
-	    m_key_bindings[key](this);
-	    return 1;
-	}
-    }
-    return Fl_Text_Editor::handle(event);
 }
 
 /* Disables default behavior of pressing ESC causing the program to close */
@@ -75,20 +59,41 @@ static int disable_escape_handler(int event)
     return 0;
 }
 
+/* Confirms that the user want to quit the editor */
+static int ask_before_closing(int event)
+{
+    // "Yes" -> 0, "No" -> 1
+    if(event == FL_SHORTCUT && Fl::event_state() == FL_COMMAND
+       && Fl::event_key() == 'q'
+       && fl_choice("Are you sure you want to quit?", "Yes", "No", 0) != 0)
+	return 1;
+    return 0;
+}
+
 int main(int argc, char **argv)
 {
     Fl::scheme("gtk+"); //none / plastic / gleam
+    // macOS won't return from Fl::run() otherwise
+    #ifdef __APPLE__
+      fl_mac_quit_early = 0;
+    #endif
     Fl::add_handler(disable_escape_handler);
+    Fl::add_handler(ask_before_closing);
     /*const Fl_Text_Display::Style_Table_Entry Style_Table[] = {
 	{ FL_BLACK,       font, FL_NORMAL_SIZE  }, // Plain text
 	{ FL_DARK_YELLOW, font, FL_NORMAL_SIZE  } // Comments
 	};*/
-    auto *window = new Fl_Window(Editor::Width, Editor::Width);
+    auto *window = new Fl_Window(Width, Width);
+    const Fl_Font font = initFonts();
     window->begin();
-        auto *editor = new Editor(0, 0, "test.txt");
+        auto *editor = new Fl_Text_Editor(0, 0, Width, Width);
+        editor->buffer(new Fl_Text_Buffer(Initial_Buffer_Size));
 	window->resizable(editor);
+	auto *buffer = initEditor(editor);
     window->end();
 
     window->show(argc, argv);
-    return Fl::run();
+    int status = Fl::run();
+    delete buffer;
+    return status;
 }
